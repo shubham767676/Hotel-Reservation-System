bb987f13bfbd87de9d83a448b3a5c5e4c1375b7d
“shubham767676” <shubhamv.singh06@gmail.com>
2022-03-14 10:26:44 +0530

    Added: Refactored

--- main/java/com/bridgelabz/HotelReservationClass/HotelReservationClass.java
+++ main/java/com/bridgelabz/HotelReservationClass/HotelReservationClass.java
@@ -6,10 +6,19 @@ package com.bridgelabz.HotelReservationClass;
  */
 
 public class HotelReservationClass {
+	public Hotel addHotel(String hotelName, double weekDayRate) {
+		Hotel hotel = new Hotel(); // New Constructor
+		hotel.setHotelName(hotelName);
+		hotel.setWeekdayRegularCustomerCost(weekDayRate);
+		return hotel;
+	}
+
+	Hotel hotel = new Hotel("Lakewood", 110, 0.0); // New Hotel added
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Hotel Reservation Program"); // Displaying welcome message
-
+		// System.out.println(hotel);
+		System.out.println(Hotel);
 	}
 
 }

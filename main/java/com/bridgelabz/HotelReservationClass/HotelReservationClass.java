caaf7bcbc9466aa97ab5fda58364add856e886d8
“shubham767676” <shubhamv.singh06@gmail.com>
2022-03-14 08:53:33 +0530

    Added: Welcome Message

---
+++ main/java/com/bridgelabz/HotelReservationClass/HotelReservationClass.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.HotelReservationClass;
+
+/* @purpose: Hotel Reservation System
+ * @file: HotelReservationClass.java
+ * @author: Shubham Singh
+ */
+
+public class HotelReservationClass {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservation Program"); // Displaying welcome message
+
+	}
+
+}
